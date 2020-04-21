b360391a77b54d320287fd05683c0438eeeee077
anthonyca18m <anthonyca18m@gmail.com>
2020-04-21 13:18:38 -0500

    agregado dos components : header, footer command: ng g c shared/nombre_archivo

--- myportfolio/src/app/app.module.ts
+++ myportfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './shared/headers/headers.component';
+import { FootersComponent } from './shared/footers/footers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    FootersComponent
   ],
   imports: [
     BrowserModule,
