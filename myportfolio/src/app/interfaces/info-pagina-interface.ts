a1dfe6a9cdd6fa9c2a33e4af06d98c0d721d15be
anthonyca18m <anthonyca18m@gmail.com>
2020-04-22 12:27:42 -0500

    todo el home esta establecido con ws de firebase

--- myportfolio/src/app/interfaces/info-pagina-interface.ts
+++ myportfolio/src/app/interfaces/info-pagina-interface.ts
@@ -1,11 +1,31 @@
+export interface RootObject {
+  body?: Body;
+  footer?: Footer;
+}
 
-export interface infoPaginaI{
+export interface Footer {
+  author?: Author;
+  redesociales?: Redesociales;
+}
 
-    Facebook?: string;
-    Github?: string;
-    Gmail?: string;
-    Instagram?: string;
-    Linkedin?: string;
-    Youtube?: string;
+export interface Redesociales {
+  Facebook?: string;
+  Github?: string;
+  Gmail?: string;
+  Instagram?: string;
+  Linkedin?: string;
+  Youtube?: string;
+}
 
+export interface Author {
+  link?: string;
+  name?: string;
+}
+
+export interface Body {
+  anexo_call?: string;
+  call?: number;
+  carrer?: string;
+  experience_old?: string;
+  name?: string;
 }
--- myportfolio/src/app/services/info-pagina.service.ts
+++ myportfolio/src/app/services/info-pagina.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { infoPaginaI } from '../interfaces/info-pagina-interface';
+import { Redesociales, Author, Body } from '../interfaces/info-pagina-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
   
-  info : infoPaginaI = {};
+  info_rs : Redesociales = {};
+  author : Author = {};
+  body : Body = {};
+  celular = null;
+
   cargada = false;
 
   constructor( private http: HttpClient ) { 
@@ -15,10 +19,25 @@ export class InfoPaginaService {
     //Leer con peticiones HTTP
 
     this.http.get('https://myportfolio-59f09.firebaseio.com/infopagina/footer/redesociales.json')
-    .subscribe( (resp : infoPaginaI) => {
+    .subscribe( (resp : Redesociales) => {
+        
+      this.cargada = true;
+      this.info_rs = resp;
+    });
+
+    this.http.get('https://myportfolio-59f09.firebaseio.com/infopagina/footer/author.json')
+    .subscribe( (resp : Author) => {
 
       this.cargada = true;
-      this.info = resp;
+      this.author = resp;
+    });
+
+    this.http.get('https://myportfolio-59f09.firebaseio.com/infopagina/body.json')
+    .subscribe( (resp : Body) => {
+
+      this.cargada = true;
+      this.body = resp;
+      this.celular = "tel:" + resp.anexo_call + "" + resp.call;
     });
 
   }
